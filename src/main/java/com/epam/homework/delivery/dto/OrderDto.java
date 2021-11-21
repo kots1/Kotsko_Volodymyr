8d2af673dfa69d00b12358b30561b4aa5dbc3555
DESKTOP-6PMIQ8Q\kotsk <kotsko10@gmail.com>
2021-11-21 20:12:17 +0200

    add OrderMapper

--- src/main/java/com/epam/homework/delivery/dto/OrderDto.java
+++ src/main/java/com/epam/homework/delivery/dto/OrderDto.java
@@ -1,22 +1,24 @@
 package com.epam.homework.delivery.dto;
 
+import com.epam.homework.delivery.model.Direction;
+import com.epam.homework.delivery.model.Tariff;
+import com.epam.homework.delivery.model.User;
 import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
 @Data
 @Builder
 public class OrderDto {
-    @NotBlank
-    private String tariff;
-    @NotBlank
-    private String login;
-    @NotBlank
-    private String startCity;
-    @NotBlank
-    private String finalCity;
+    @NotNull
+    private Tariff tariff;
+    @NotNull
+    private User user;
+    @NotNull
+    private Direction direction;
     private double price;
     @Positive
     private double volume;
